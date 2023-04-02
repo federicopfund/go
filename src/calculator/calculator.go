3746c4979e71aa27a0a9f598ee22c5c8743341b6
federicopfund <federicopfund@gmail.com>
2023-04-01 22:37:44 -0300

    Calculadora

--- src/calculator/calculator.go
+++ src/calculator/calculator.go
@@ -1,5 +1,12 @@
 package calculator
 
+
+var logMessage = "[LOG]"
+
+// Version of the calculator
+var Version = "1.0"
+
+
 func Add(a, b int) int {
 	return a + b
 }
@@ -17,4 +24,14 @@ func Divide(a, b int) float64 {
 		return float64(0)
 	}
 	return float64(a) / float64(b)
+}
+
+
+func internalSum(number int) int {
+    return number - 1
+}
+
+// Sum two integer numbers
+func Sum(number1, number2 int) int {
+    return number1 + number2
 }
