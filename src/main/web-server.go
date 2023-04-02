8660aa3102261ee3cdb8d746d5af7d9dea9723fa
federicopfund <federicopfund@gmail.com>
2023-04-02 19:04:59 -0300

    Web server.

--- src/main/web-server.go
+++ src/main/web-server.go
@@ -2,14 +2,12 @@ package main
 
 /// Uncomment to use the standar library///
 import (
-	"fmt"
-	"net/http"
-	"os"
+	"github.com/gin-gonic/gin"
 )
 
 //"fmt"
 //"net/http"
-//"github.com/gin-gonic/gin"
+
 //"github.com/gofiber/fiber/v2"
 
 //func main(){
@@ -27,15 +25,15 @@ import (
 
 ///// USE Gin FRAMEWORK
 
-// func main() {
-// 	r := gin.Default()
-// 	r.GET("/", handler)
-// 	r.Run(":8080")
-// }
+func main() {
+ 	r := gin.Default()
+ 	r.GET("/", handler)
+ 	r.Run(":8080")
+ }
 
-// func handler(c *gin.Context) {
-// 	c.String(200, "Hello, World")
-// }
+ func handler(c *gin.Context) {
+ 	c.String(200, "Hello, World")
+ }
 
 // Uncomment to use fiber
 
@@ -50,25 +48,24 @@ import (
 // }
 
 
-func main(){
-	http.HandleFunc("/",handler)
-	http.ListenAndServe(":8080",nil)
+// func main(){
+// 	http.HandleFunc("/",handler)
+// 	http.ListenAndServe(":8080",nil)
 
-}
+// }
 
-func handler(w http.ResponseWriter, r *http.Request){
-	//Leer el contenido del archivo HTML
-	html, err := os.ReadFile("index.html")
-	if err != nil {
-		http.Error(w,"No se puede cargar el archivo HTML", http.StatusInternalServerError)
-		return
-	}
+// func handler(w http.ResponseWriter, r *http.Request){
+// 	//Leer el contenido del archivo HTML
+// 	html, err := os.ReadFile("index.html")
+// 	if err != nil {
+// 		http.Error(w,"No se puede cargar el archivo HTML", http.StatusInternalServerError)
+// 		return
+// 	}
 
-	// Estableca el encabeado del tipo de contenido en HTML
-	w.Header().Set("Content-Type","text/html")
+// 	// Estableca el encabeado del tipo de contenido en HTML
+// 	w.Header().Set("Content-Type","text/html")
 
-	// Escriba el archivo HTML en la respuesta
+// 	// Escriba el archivo HTML en la respuesta
 
-	fmt.Fprintf(w,"%s",html)
-
-}
+// 	fmt.Fprintf(w,"%s",html)
+// }
